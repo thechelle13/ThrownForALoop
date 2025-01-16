f10b7231fa6a96c02f1198090da4a1b8bd959777
Michelle Totherow <tym4me13@gmail.com>
2025-01-16 09:44:44 -0600

    continued progress through NSS c# circ

---
+++ Product.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Product
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public bool Sold { get; set; }
+    public DateTime StockDate { get; set; }
+    public int ManufactureYear { get; set; }
+}
